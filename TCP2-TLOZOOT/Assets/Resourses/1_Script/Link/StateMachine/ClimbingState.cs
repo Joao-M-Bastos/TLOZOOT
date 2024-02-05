b4261a6b88fa769cbe6d9dce9a07c66aafd4c3a5
Joao-M-Bastos <jmb32180@gmail.com>
2024-02-05 16:45:56 -0300

    SomeUpdates

--- TCP2-TLOZOOT/Assets/Resourses/1_Script/Link/StateMachine/ClimbingState.cs
+++ TCP2-TLOZOOT/Assets/Resourses/1_Script/Link/StateMachine/ClimbingState.cs
@@ -39,5 +39,66 @@ public class ClimbingState : BaseState
         {
             machineController.ChangeState(machineController.groundedState);
         }
+        if (CanClimbCorner())
+        {
+
+        }
+    }
+
+    public void CanClimbCorner()
+    {
+        if (!RayAcimaFrontal())
+        {
+            if (RayFrontal() && RayAcima())
+            {
+                GetInCorner();
+            }
+        }
+    }
+
+    public bool RayFrontal()
+    {
+        //Verifica se há uma parede escalavel a frente
+        Vector3 rayStartPos = this.transform.position + new Vector3(0, 2f, 0);
+
+        //Debug.DrawRay(rayStartPos, instaciaPlayer.PlayerForword * 1.4f, Color.green);
+
+        if (Physics.Raycast(rayStartPos, instaciaPlayer.PlayerForword, out rayFrontal, 1.2f, instaciaPlayer.ClimbLayerMask)
+         || Physics.Raycast(rayStartPos, instaciaPlayer.PlayerForword, out rayFrontal, 1.2f, instaciaPlayer.GroundLayerMask))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool RayAcima()
+    {
+
+        //Verifica se há uma parede escalavel a frente
+        Vector3 rayStartPos = this.transform.position + instaciaPlayer.PlayerForword * 1.5f + new Vector3(0f, 5f, 0f);
+
+        //Debug.DrawRay(rayStartPos, -this.transform.up * 2.4f, Color.green);
+
+        if (Physics.Raycast(rayStartPos, -this.transform.up, out rayAcima, 2.4f, instaciaPlayer.ClimbLayerMask)
+        || Physics.Raycast(rayStartPos, -this.transform.up, out rayAcima, 2.4f, instaciaPlayer.GroundLayerMask))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool RayAcimaFrontal()
+    {
+        //Verifica se há uma parede escalavel a frente
+        Vector3 rayStartPos = this.transform.position + new Vector3(0f, 4, 0f);
+
+        Debug.DrawRay(rayStartPos, instaciaPlayer.PlayerForword * 2.4f, Color.green);
+
+        if (Physics.Raycast(rayStartPos, instaciaPlayer.PlayerForword, out rayAcimaFrontal, 2.4f, instaciaPlayer.ClimbLayerMask)
+        || Physics.Raycast(rayStartPos, instaciaPlayer.PlayerForword, out rayAcimaFrontal, 2.4f, instaciaPlayer.GroundLayerMask))
+        {
+            return true;
+        }
+        return false;
     }
 }
--- TCP2-TLOZOOT/Assets/Resourses/1_Script/Link/StateMachine/CornerState.cs
+++ TCP2-TLOZOOT/Assets/Resourses/1_Script/Link/StateMachine/CornerState.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class CornerState : BaseState
 {
+    public override void EndState(LinkScpt link, StateMachineController machineController)
+    {
+        throw new System.NotImplementedException();
+    }
 
+    public override void EnterState(LinkScpt link, StateMachineController machineController)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void FixedUpdateState(LinkScpt link, StateMachineController machineController)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateState(LinkScpt link, StateMachineController machineController)
+    {
+        throw new System.NotImplementedException();
+    }
 }
