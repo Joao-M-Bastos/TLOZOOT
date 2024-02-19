0ec4e95482822240cf9c26c75b411831dbba27f3
Joao-M-Bastos <jmb32180@gmail.com>
2024-02-19 14:30:56 -0300

    RightHandHolder

--- TCP2-TLOZOOT/Assets/Resourses/1_Script/Weapons/BaseMelle.cs
+++ TCP2-TLOZOOT/Assets/Resourses/1_Script/Weapons/BaseMelle.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class BaseMelle : MonoBehaviour
+public abstract class BaseMelle : MonoBehaviour
 {
     [SerializeField] float cooldownBase, knockback;
-    [SerializeField] int damage, id, type;
+    [SerializeField] int damage, id, type, maxCombo;
+    [SerializeField] int extra1, extra2, extra3;
 
     private float cooldown, comboCount;
 
+    public abstract void Attack();
 
+    public abstract void CanAttack();
 
-    // Update is called once per frame
-    void Update()
+    public bool IsCooldownZero()
     {
-        
+        return cooldown == 0;
+    }
+
+    public void ResetCooldown()
+    {
+        cooldown = cooldownBase;
+    }
+
+    public void AddCombo()
+    {
+        comboCount++;
+
+        if(comboCount > maxCombo)
+            comboCount = 0;
     }
 }
